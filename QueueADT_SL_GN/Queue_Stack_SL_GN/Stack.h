7f0e989d03192d59514061659570478880e7089a
Santosh Lakshman <lathalaxsan@gmail.com>
2020-02-11 19:29:02 -0800

    Added Stack

---
+++ QueueADT_SL_GN/Queue_Stack_SL_GN/Stack.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "SinglyLinkedList.h"
+/*
+	Lab 3: Queue1 by Santosh Lakshman and Gokul Nair
+*/
+template<class T>
+class Stack : protected SinglyLinkedList<T> {
+public:
+	Stack();
+	void push(T*);
+	T* pop();
+	T* peek();
+	bool empty();
+};
+
+
+/* Creates an empty Stack object.
+ * Pre: None
+ * Post: Inherited sort member is set to UNSORTED by default */
+template<class T>
+Stack<T>::Stack() {
+	SinglyLinkedList<T>::sort = UNSORTED;
+}
+
+
+/* Adds a new value to the top of the stack
+ * Pre: Stack exists
+ * Post: A new element is added to the stack */
+template<class T>
+void Stack<T>::push(T* value) {
+	SinglyLinkedList<T>::add(value);
+}
+
+
+/* Removes the top element from the stack and returns it
+ * Pre: Stack exists
+ * Post: Returns true if element is removed successfully */
+template<class T>
+T* Stack<T>::pop() {
+	T* returnVal = (SinglyLinkedList<T>::tail)->getVal();
+	SinglyLinkedList<T>::remove(*((SinglyLinkedList<T>::tail)->getVal()));
+	return returnVal;
+}
+
+
+/* Returns the value at the top of the stack
+ * Pre: Stack exists
+ * Post: Returns a pointer to the value in the tail node. */
+template<class T>
+T* Stack<T>::peek() {
+	return (SinglyLinkedList<T>::tail)->getVal();
+}
+
+
+/* Returns if the Stack is empty or not.
+ * Pre: Stack exists
+ * Post: Returns false if the head pointer point to anything and true otherwise */
+template<class T>
+bool Stack<T>::empty() {
+	return SinglyLinkedList<T>::tail == nullptr;
+};
+
+
+
---
+++ QueueADT_SL_GN/Queue_Stack_SL_GN/main.cpp
@@ -0,0 +1,43 @@
+#include "Rupee.h"
+#include "Queue.h"
+#include "Stack.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Lab 3: Queue1 by Santosh Lakshman and Gokul Nair\n";
+
+	Queue<int> queue1 = Queue<int>();
+	cout << "\nIntegers in the order in which they inserted into the queue and stack: " << endl;
+	int* intArr[10];
+	intArr[0] = new int(9);
+	intArr[1] = new int(3);
+	intArr[2] = new int(4);
+	intArr[3] = new int(1);
+	intArr[4] = new int(10);
+	intArr[5] = new int(7);
+	intArr[6] = new int(5);
+	intArr[7] = new int(2);
+	intArr[8] = new int(8);
+	intArr[9] = new int(6);
+	for (int i = 0; i < 10; i++) {
+		cout << *intArr[i] << endl;
+		queue1.enqueue(intArr[i]);
+	}
+	cout << "Queue front: " << *(queue1.front()) << endl;
+	cout << "Queue rear: " << *(queue1.rear()) << endl;
+	queue1.dequeue();
+	cout << "Queue front: " << *(queue1.front()) << endl;
+	cout << "Queue rear: " << *(queue1.rear()) << endl;
+
+	Stack<int> stack1 = Stack<int>();
+	for (int i = 0; i < 10; i++) {
+		stack1.push(intArr[i]);
+	}
+	cout << "Stack top: " << *(stack1.peek()) << endl;
+	cout << "Popping: " << *(stack1.pop()) << endl;
+	cout << "Stack top: " << *(stack1.peek()) << endl;
+
+}
